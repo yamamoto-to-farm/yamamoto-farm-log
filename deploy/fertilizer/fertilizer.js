1bf28d550df94cf7c340d02b8404403df75732b4
yamamoto-to-farm <yamamoto.to.farm@gmail.com>
2026-05-10 23:09:05 +0900

    Update fertilizer.js

--- deploy/fertilizer/fertilizer.js
+++ deploy/fertilizer/fertilizer.js
@@ -10,7 +10,7 @@ function debugLog(...args) {
 }
 
 import { openFieldModal } from "/common/filter/filter-field.js?v=2";
-import { setFilterData, filterState } from "/common/filter/filter-core.js?v=1";
+import { setFilterData, filterState } from "/common/filter/filter-core.js?v=2";
 import { initActiveFilterUI } from "/common/filter/filter-active.js?v=1";
 import { saveMultiFieldLog } from "/common/general-log/base.js?v=1";
 
