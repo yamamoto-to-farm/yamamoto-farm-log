42032e5c60de80754aa8049fe44538f55d84a110
yamamoto-to-farm <yamamoto.to.farm@gmail.com>
2026-05-18 01:55:57 +0900

    修正

--- deploy/common/print/print.js
+++ deploy/common/print/print.js
@@ -5,18 +5,23 @@ window.addEventListener("message", (e) => {
     const { title, html } = e.data;
 
     document.title = title;
-    document.getElementById("print-root").innerHTML = `
+    const root = document.getElementById("print-root");
+
+    root.innerHTML = `
       <h1>${title}</h1>
       ${html}
     `;
 
-    // 折りたたみ強制展開
+    // ★ 折りたたみ強制展開
     document.querySelectorAll(".field-group > div").forEach(w => {
       w.style.display = "block";
     });
 
-    window.print();
-    window.close();
+    // ★ レイアウト確定を待ってから印刷
+    setTimeout(() => {
+      window.print();
+      window.close();
+    }, 150); // ← これが超重要
   }
 });
 
